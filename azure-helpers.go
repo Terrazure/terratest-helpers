8289b49ecd3770cfe0bcfe54063760e5c51a6b0d
Pinaki <pinaki@pinakis.co.in>
2023-08-20 23:41:08 +0530

    Fix: go mod parsing issue

--- azure-helpers.go
+++ azure-helpers.go
@@ -1,11 +1,16 @@
-package terratest_helpers
+package terratest_helpers //import github.com/Terrazure/terratest-helpers
 
 import (
 	"context"
+	"fmt"
 	"github.com/Azure/go-autorest/autorest"
 	"github.com/gruntwork-io/terratest/modules/azure"
+	"path/filepath"
+	"strings"
 	"testing"
 	"time"
+
+	"github.com/otiai10/copy"
 )
 
 func GetAuthorizer(t *testing.T) autorest.Authorizer {
@@ -27,4 +32,17 @@ func ConfigureAzureResourceClient(t *testing.T, client *autorest.Client) {
 
 func BuildDefaultHttpContext() (context.Context, context.CancelFunc) {
 	return context.WithTimeout(context.Background(), 10*time.Minute)
+}
+
+var copyOptions = copy.Options{
+	Skip: func(src string) (bool, error) {
+		return filepath.Ext(src) != ".tf", nil
+	},
+}
+
+func PrepareTerraformParallelTestingDir(source string, contextName string, testCaseIndex int) string {
+	originalTerraformDir, _ := filepath.Abs(fmt.Sprintf("%s", source))
+	parallelTerraformDir, _ := filepath.Abs(fmt.Sprintf("%s-%s-%v", source, strings.ToLower(contextName), testCaseIndex))
+	copy.Copy(originalTerraformDir, parallelTerraformDir, copyOptions)
+	return parallelTerraformDir
 }
